a94a355950fd21cafd3cbd47d73e5acdc38b76bb
wayfarer-ismail <ismailelbahja03@gmail.com>
2023-03-28 17:37:37 +0100

    updated db schema

--- src/viewmodels/MainThread.java
+++ src/viewmodels/MainThread.java
@@ -5,6 +5,8 @@ import services.DatabaseInfo;
 public class MainThread {
 
     public static void main(String[] args) {
+        DatabaseInfo.loadDriver();
+        DatabaseInfo.testDatabaseConnection();
         CommunityNewsForum newsForum = new CommunityNewsForum();
         newsForum.start();
     }
